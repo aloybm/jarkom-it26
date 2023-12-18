9d930be0311229483dbc6997888998495feb78b6
aloybm <manullangaloysius@gmail.com>
2023-12-19 00:52:25 +0700

    code

--- Modul 5/script/dhcprelay.sh
+++ Modul 5/script/dhcprelay.sh
@@ -1,5 +1,4 @@
 # Heiter Himmel Frieren
-
 echo 'nameserver 192.168.122.1' > /etc/resolv.conf
 
 apt update
@@ -11,9 +10,5 @@ SERVERS="192.246.1.106"
 INTERFACES="eth0 eth1 eth2 eth3"
 OPTIONS=""
 ' > /etc/default/isc-dhcp-relay
-
-# Jangan lupa uncomment 
-# nano /etc/sysctl.conf
-# net.ipv4.ip_forward=1
 
 service isc-dhcp-relay restart
